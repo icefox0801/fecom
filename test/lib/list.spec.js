ced20b3349d6a04cdab7560add54357c84fb8349
icefox0801 <icefox0801@hotmail.com>
2016-06-16 15:27:40 +0800

    #55 #56 bug修复

--- test/lib/list.spec.js
+++ test/lib/list.spec.js
@@ -1,86 +1,135 @@
 'use strict';
 
+var path = require('path');
+
+var fs = require('graceful-fs');
+
 var fecom = require('../../lib/fecom');
 var list = require('../../lib/list');
+var reset = require('../helpers/reset');
 
 describe('list', function () {
   describe('installed', function () {
-    it('should return valid installed components', function (done) {
-      var expectedInstalled = [
-        {
-          name: 'comp_deps',
-          owner: 'icefox0801',
-          version: '1.0.0',
-          hasUpdate: false,
-          latest: ''
-        },
-        {
-          name: 'comp_sub_a',
-          owner: 'icefox0801',
-          version: '1.1.0',
-          hasUpdate: false,
-          latest: ''
-        },
-        {
-          name: 'comp_sub_b',
-          owner: 'icefox0801',
-          version: '1.0.1',
-          hasUpdate: false,
-          latest: ''
-        },
-        {
-          name: 'comp_valid_version',
-          owner: 'icefox0801',
-          version: '1.1.2',
-          hasUpdate: false,
-          latest: ''
-        }
-      ];
-      list([], { cwd: fecom.root })
-        .then(function (installed) {
-          expect(installed).toEqual(expectedInstalled);
-          done();
-        });
+    describe('with component.json', function () {
+      it('should return valid installed components', function (done) {
+        var expectedInstalled = [
+          {
+            name: 'comp_deps',
+            owner: 'icefox0801',
+            version: '1.0.0',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_sub_a',
+            owner: 'icefox0801',
+            version: '1.1.0',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_sub_b',
+            owner: 'icefox0801',
+            version: '1.0.1',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_valid_version',
+            owner: 'icefox0801',
+            version: '1.1.2',
+            hasUpdate: false,
+            latest: ''
+          }
+        ];
+        list([], { cwd: fecom.root })
+          .then(function (installed) {
+            expect(installed).toEqual(expectedInstalled);
+            done();
+          });
+      });
+    });
+    describe('without component.json', function () {
+      it('should return valid installed components', function (done) {
+        var expectedInstalled = [
+          {
+            name: 'comp_deps',
+            owner: 'icefox0801',
+            version: '1.0.0',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_sub_a',
+            owner: 'icefox0801',
+            version: '1.1.0',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_sub_b',
+            owner: 'icefox0801',
+            version: '1.0.1',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_valid_version',
+            owner: 'icefox0801',
+            version: '1.1.2',
+            hasUpdate: false,
+            latest: ''
+          }
+        ];
+        fs.unlinkSync(path.join(fecom.root, 'component.json'));
+        list([], { cwd: fecom.root })
+          .then(function (installed) {
+            expect(installed).toEqual(expectedInstalled);
+            reset();
+            done();
+          });
+      });
     });
   });
-
   describe('installed update', function () {
-    it('should return valid installed components', function (done) {
-      var expectedInstalled = [
-        {
-          name: 'comp_deps',
-          owner: 'icefox0801',
-          version: '1.0.0',
-          hasUpdate: false,
-          latest: ''
-        },
-        {
-          name: 'comp_sub_a',
-          owner: 'icefox0801',
-          version: '1.1.0',
-          hasUpdate: true,
-          latest: '1.1.1'
-        },
-        {
-          name: 'comp_sub_b',
-          owner: 'icefox0801',
-          version: '1.0.1',
-          hasUpdate: false,
-          latest: ''
-        },
-        {
-          name: 'comp_valid_version',
-          owner: 'icefox0801',
-          version: '1.1.2',
-          hasUpdate: false,
-          latest: ''
-        }
-      ];
-      list([], { cwd: fecom.root, update: true })
-        .then(function (installed) {
-          expect(installed).toEqual(expectedInstalled);
-          done();
-        });
+    describe('with component.json', function () {
+      it('should return valid installed components', function (done) {
+        var expectedInstalled = [
+          {
+            name: 'comp_deps',
+            owner: 'icefox0801',
+            version: '1.0.0',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_sub_a',
+            owner: 'icefox0801',
+            version: '1.1.0',
+            hasUpdate: true,
+            latest: '1.1.1'
+          },
+          {
+            name: 'comp_sub_b',
+            owner: 'icefox0801',
+            version: '1.0.1',
+            hasUpdate: false,
+            latest: ''
+          },
+          {
+            name: 'comp_valid_version',
+            owner: 'icefox0801',
+            version: '1.1.2',
+            hasUpdate: false,
+            latest: ''
+          }
+        ];
+        list([], { cwd: fecom.root, update: true })
+          .then(function (installed) {
+            expect(installed).toEqual(expectedInstalled);
+            done();
+          });
+      });
     });
   });
 });
